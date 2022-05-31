2aec9c0b9164a9739302b507e856130805f3fd11
vraimademoiselle <mademoisellebone@gmail.com>
2022-05-31 18:46:37 +0300

    second file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+for ch in range(97, 123):
+    if ch != 101 and ch != 113:
+        print("{:c}".format(ch), end='')
+    

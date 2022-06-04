bdf113c313dd19fda717f9b440a31cde1356f435
vraimademoiselle <mademoisellebone@gmail.com>
2022-06-04 12:02:13 +0300

    none chmoding files

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,27 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}

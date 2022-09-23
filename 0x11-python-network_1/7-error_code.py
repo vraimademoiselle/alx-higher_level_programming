6cdb601e9dba54bec21e84290995417558490cb9
vraimademoiselle <mademoisellebone@gmail.com>
2022-09-23 19:50:38 +0300

    python

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/python3
-"""A  script that:
-- takes in a URL and an email address
-- sends a POST request to the passed URL with the email as a parameter
+"""A script that
+- takes in a URL
+- sends a request to the URL
 - displays the body of the response.
 """
 import sys
@@ -11,7 +10,9 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)

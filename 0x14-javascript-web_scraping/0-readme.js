a14c074cad0e3913a44b0b05c0f224588daad09a
vraimademoiselle <mademoisellebone@gmail.com>
2022-10-19 23:39:19 +0300

    first file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation

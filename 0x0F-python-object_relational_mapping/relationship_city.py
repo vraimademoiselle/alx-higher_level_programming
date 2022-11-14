83f7a5d2894d2c53f42af55bafaffd5f65b03491
vraimademoiselle <mademoisellebone@gmail.com>
2022-11-14 13:35:21 +0300

    Improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py:

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Definition of the State class with relationship to City"""
+"""Definition of the City class"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from relationship_city import City, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
 
 
-class State(Base):
-    """Class State"""
+class City(Base):
+    """Class City"""
 
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True,
                 primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
